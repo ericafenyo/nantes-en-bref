0d3df9fe523f45da3fe06b6b5f3f5a97572bea10
ericafenyo <ericafenyo@yahoo.com>
2019-03-06 12:49:26 +0100

    code update

--- contact.js
+++ contact.js
@@ -1,8 +1,36 @@
 
 $(document).ready(() => {
-
+    $('#submit-button').click((event) => {
+        //Prevent default onClick event behaviour.
+        alert("Hello")
+        // event.preventDefault()
+        // const inputs = readInputs()
+        // alert(inputs)
+    })
 });
 
-const readInputs = ()=>{
-    
+/**
+ * Read `form` inputs
+ */
+const readInputs = () => {
+    const fullName = $('#input-name').val()
+    const emialAddress = $('#input-email').val()
+    const message = $('#input-message').val()
+
+    if (fullName && emialAddress && message) {
+        const info = {
+            name: fullName,
+            emial: emialAddress,
+            message: message
+        }
+
+        return info
+    } else {
+        notifyError("Please fill all the fields")
+    }
+}
+
+//displays a dialog box with `message` as text.
+const notifyError = (message) => {
+    alert(message)
 }
