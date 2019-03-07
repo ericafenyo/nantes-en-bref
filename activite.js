2c23342a12bdf070c8bfda9169016fdc74ca841e
ericafenyo <ericafenyo@yahoo.com>
2019-03-07 17:50:08 +0100

    deleted unnecessary lines

--- activite.js
+++ activite.js
@@ -1,41 +1,7 @@
 $(document).ready(() => {
-    const Chart = require('chart.js');
-    
-    const context = $("#chart")
-    const ratingBar = new Chart(context, {
-        type: 'bar',
-        data: {
-            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
-        }
-    });
-})
+    const titleElement = $('#activity-title')
+    const addressElement = $('#activity-address')
+    $('.list-item').click(() => {
+        console.log(this.tagName)
+    })
+});
