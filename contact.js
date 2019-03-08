91c77c057999d603e3fc849d2938ce2f3da38e22
ericafenyo <ericafenyo@yahoo.com>
2019-03-08 05:36:53 +0100

    correct form alert message

--- contact.js
+++ contact.js
@@ -1,41 +1,43 @@
-
 $(document).ready(() => {
-    $('#submit-button').click((event) => {
-        //Prevent default onClick event behaviour.
-        event.preventDefault()
-        readInputs()
-    })
+  $("#submit-button").click(event => {
+    //Prevent default onClick event behaviour.
+    event.preventDefault();
+    readInputs();
+  });
 });
 
 /**
  * Read `form` inputs
  */
 const readInputs = () => {
-    const fullName = $('#input-name').val()
-    const emialAddress = $('#input-email').val()
-    const message = $('#input-message').val()
-    const alertCarrier = $('#alert')
+  const fullName = $("#input-name").val();
+  const emialAddress = $("#input-email").val();
+  const message = $("#input-message").val();
+  const alertCarrier = $("#alert");
 
-    /* If all values are truthy, proceed to print them, else display an error messsage.*/
-    if (fullName && emialAddress && message) {
-        const info = {
-            name: fullName,
-            emial: emialAddress,
-            message: message
-        }
-        const message = `FullName: ${info.name} Email: ${info.emial} Message: ${info.message}`
-        alertCarrier.text(message)
-        setTimeout(() => {
-            alertCarrier.addClass('d-none')
-        }, 5000)
-        // return info
-    } else {
-        alertCarrier.removeClass('d-none')
-        alertCarrier.removeClass('alert-success')
-        alertCarrier.addClass('alert-danger')
-        alertCarrier.text("Please fill all the fields")
-        setTimeout(() => {
-            alertCarrier.addClass('d-none')
-        }, 5000)
-    }
-}
+  /* If all values are truthy, proceed to print them, else display an error messsage.*/
+  if (fullName && emialAddress && message) {
+    const info = {
+      name: fullName,
+      emial: emialAddress,
+      message: message
+    };
+    const alertMessage = `${fullName}: Your information has been submitted successfully.`;
+    alertCarrier.removeClass("opacity-0");
+    alertCarrier.removeClass("alert-danger");
+    alertCarrier.addClass("alert-success");
+    alertCarrier.text(alertMessage);
+    setTimeout(() => {
+      alertCarrier.addClass("opacity-0");
+    }, 5000);
+    // return info
+  } else {
+    alertCarrier.removeClass("opacity-0");
+    alertCarrier.removeClass("alert-success");
+    alertCarrier.addClass("alert-danger");
+    alertCarrier.text("Please fill all the fields");
+    setTimeout(() => {
+      alertCarrier.addClass("opacity-0");
+    }, 5000);
+  }
+};
